8c60a57deb571d8d53de253f10b9af370a035614
Raj Randive <randiveraj96@gmail.com>
2022-08-05 11:29:35 +0530

    Passed in first trial.

---
+++ 28-07-2022/Program3_Input_and_Operation.java
@@ -0,0 +1,14 @@
+
+// import java.util.*;
+public class Program3_Input_and_Operation {
+    public static void main(String args[]){
+
+        int a = 3, b= 5;
+        System.out.println("The addition of a and b is : " +(a+b));
+        System.out.println("The addition of a and b is : " +(a-b));
+        System.out.println("The addition of a and b is : " +(a*b));
+        System.out.println("The addition of a and b is : " +(a/b));
+        
+        
+    }
+}
