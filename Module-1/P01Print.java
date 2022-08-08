b13f8aa2d3575153c14deb9b0bfd4f438ffbe9a2
Raj Randive <randiveraj96@gmail.com>
2022-08-08 15:49:32 +0530

    initial commit

---
+++ Module-1/P01Print.java
@@ -0,0 +1,7 @@
+public class P01Print {
+    public static void main(String[] args) {
+    
+		System.out.println("\nHello Raj !!\n");
+
+    }
+}
---
+++ Module-1/P03SimpleOperations.java
@@ -0,0 +1,12 @@
+public class P03SimpleOperations {
+    public static void main(String args[]){
+
+        int a = 3, b= 5;
+        System.out.println("The addition of a and b is : " +(a+b));
+        System.out.println("The addition of a and b is : " +(a-b));
+        System.out.println("The addition of a and b is : " +(a*b));
+        System.out.println("The addition of a and b is : " +(a/b));
+        
+        
+    }
+}
