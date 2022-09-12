408c12afbb902d7d27909e3afa7ac7769783af4e
Raj Randive <randiveraj96@gmail.com>
2022-09-12 21:47:34 +0530

    initial commit

---
+++ Module1/P_01/P01Print.java
@@ -0,0 +1,9 @@
+// 1. Install JDK, setup Java environment and write a program to print -“CODING IS FUN, ENJOY IT!”.
+package Module1.P_01;
+public class P01Print {
+    public static void main(String[] args) {
+    
+		System.out.println("\nCODING IS FUN, ENJOY IT!\n");
+
+    }
+}
