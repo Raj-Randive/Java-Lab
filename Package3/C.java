07ff57a8c6f9b3e126eacb8fa3dad6d4472522fe
Raj Randive <randiveraj96@gmail.com>
2022-10-15 10:01:21 +0530

    initial commit

---
+++ Package3/C.java
@@ -0,0 +1,8 @@
+package Package3;
+
+public class C{
+    public void show(){
+        System.out.println("Inside Class C from different package.");
+    }
+}
+
---
+++ Package3/D.java
@@ -0,0 +1,7 @@
+package Package3;
+
+public class D extends C {
+    public void show() {
+        System.out.println("Inside Class D from different package.");
+    }
+}
