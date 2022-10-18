0d3e9ddef961529ab6151d7d0dd3c7b3c3046843
Raj Randive <randiveraj96@gmail.com>
2022-10-18 11:34:38 +0530

    initial commit

---
+++ Module3/P_09/P09UserDefinedExceptions.java
@@ -0,0 +1,55 @@
+// 9. Write a program to create your own exception types to handle situation specific to your application (Hint: Define a subclass of Exception which itself is a subclass of Throwable).
+
+package Module3.P_09;
+import java.util.Scanner;
+
+class MobileExcep extends Exception{
+    MobileExcep(String str){
+        super(str);
+    }
+}
+
+public class P09UserDefinedExceptions {
+//
+//    static boolean lengthCheck(String s){
+//        if (s.length() != 10){
+//            return false;
+//        }
+//        else{
+//            return true;
+//        }
+//    }
+
+    static boolean characterCheck(String s){
+        long phoneNumber;
+        try{
+            phoneNumber = Long.parseLong(s);  //Converts the string to the long (Means the integer)
+            return false;
+
+        }catch(NumberFormatException e){
+            return true;
+        }
+    }
+
+    public static void main(String args[]){
+
+        Scanner myscan = new Scanner(System.in);
+
+        System.out.println("Enter the Phone Number: ");
+        String phoneNum = myscan.nextLine();
+
+        try{
+            if (phoneNum.length() != 10){
+                throw new MobileExcep("The Phone Number should contain exactly 10 digits.");
+            }
+            if(characterCheck(phoneNum)){
+                throw new MobileExcep("Enter only digits (Integer Numbers).");
+            }
+
+        }catch(MobileExcep e){
+            System.out.println(e);
+            System.out.println("\nPlease enter the phone number in correct format.");
+        }
+
+    }
+}
