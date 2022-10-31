7d5dc58bee8fb120504a695b03b4b64ca8505f84
Raj Randive <randiveraj96@gmail.com>
2022-10-31 23:31:01 +0530

    completed the code

--- JavaAssignments/Assignment_1.java
+++ JavaAssignments/Assignment_1.java
@@ -2,14 +2,69 @@ package JavaAssignments;
 import java.util.Scanner;
 
 class bank{
-    double networth = 50000;
-    static int numOfAcc = 0;
-    double cusNumber;
     String cusName;
-    double accBalance;
-    double loanSanctioned;
-    bank(){
+    double cusNumber, accBalance;
+    static int numOfAcc = 0;
+    static double networth = 50000;
+    static double loanSanctioned = 0;
+    static double loanAmount=0;
+    int numOfInstallments=0;
 
+    bank(double cusNumber, String cusName, double accBalance){
+        this.cusNumber = cusNumber;
+        this.cusName = cusName;
+        this.accBalance = accBalance;
+        numOfAcc += 1;
+        networth += accBalance;
+    }
+
+    void addMoney(double accBalance){
+        this.accBalance += accBalance;
+        networth = accBalance + networth;
+    }
+
+    void reqLoan(double amt){
+
+        loanAmount += amt;
+        if ( (loanAmount < 2*accBalance) && (loanAmount < networth) ){
+            loanSanctioned += 1;
+            networth = networth - loanAmount;
+            System.out.print("\n****************************************************\n Congrats!. Your loan is approved.\n****************************************************\n");
+        }
+        else{
+            System.out.print("\n****************************************************\n Sorry your loan can't be approved.\n****************************************************\n");
+            loanAmount = loanAmount-amt;
+        }
+    }
+
+    void repaySanctionedLoan(double amount){
+        networth = networth + amount;
+        if (amount > loanAmount){
+            accBalance = ( accBalance + (amount-loanAmount) ) ;
+            loanAmount = 0;
+        }else{
+            loanAmount = loanAmount - amount;
+        }
+        numOfInstallments += 1;
+        System.out.print("\n****************************************************\n " + amount + " paid to your sanctioned loan. \n****************************************************\n");
+    }
+
+    void inspection(){
+        System.out.println("\n****************************************************\nNetworth of bank is: " + networth);
+        System.out.println("Number of accounts in this branch: " + numOfAcc);
+        System.out.println("Total number of loans sanctioned till date: " + loanSanctioned);
+        System.out.println("Total loan amount sanctioned till date: " + loanAmount);
+        System.out.println("\n****************************************************");
+    }
+
+    void showAccDetails(){
+        System.out.println("\n****************************************************\nCustomer Number: " + cusNumber);
+        System.out.println("Name: " + cusName);
+        System.out.println("Balance: " + accBalance);
+        System.out.println("Total number of loans taken: " + loanSanctioned);
+        System.out.println("Total loan amount taken: " + loanAmount);
+        System.out.println("Number of installments made to repay your loan: " + numOfInstallments);
+        System.out.println("\n****************************************************");
     }
 
 }
@@ -19,32 +74,71 @@ public class Assignment_1 {
 
         Scanner myscan = new Scanner(System.in);
 
-        System.out.print("\nPress 'Y' to create a new account in our bank or press 'N' to exit");
-        String s = myscan.nextLine();
+        String name;
+        double accBal;
+        System.out.println("\nWelcome to our Bank. The available options are: ");
+        System.out.print("\n  > 1. Create an Account.\n  > 2. Exit");
+        System.out.print("\n\n  > Enter your choice: ");
+        int x = myscan.nextInt();
 
-        while ( !s.equals("N") ){
+        while ( x != 2 ){
+            if (x == 1) {
+                System.out.print("\nCreating your new account...");
+                System.out.print("\nEnter the customer number: ");
+                double cusNum = myscan.nextDouble();
+                System.out.print("Enter Your name: ");
+                name = myscan.next();
+                System.out.print("Enter amount to be credited in your account: ");
+                accBal = myscan.nextDouble();
 
-            switch (s){
-                case "Y" -> {
-                    System.out.print("\nCreating your new account...");
-                    System.out.print("\nEnter Your name: ");
-                    String s1 = myscan.nextLine();
-                    System.out.print("\nEnter amount to be credited in your account: ");
-                    double a1 = myscan.nextInt();
-                    break;
+                bank b1 = new bank(cusNum, name, accBal);
 
+                System.out.print("\n  > 1. Add money to your existing account.\n  > 2. Show details about your account.\n  > 3. Request for a loan.\n  > 4. Repay your loan.\n  > 5. Inspection.\n  > 6. Exit.");
+                System.out.print("\n\n  > Enter your choice: ");
+                int y = myscan.nextInt();
+
+                while(y != 6){
+                    switch (y) {
+                        case 1 -> {
+                            System.out.print("\n -> Enter amount to be added: ");
+                            double a1 = myscan.nextDouble();
+                            b1.addMoney(a1);
+                        }
+                        case 2 -> {
+                            b1.showAccDetails();
+                        }
+                        case 3 -> {
+                            System.out.print("\n -> Enter the Loan Amount: ");
+                            double z = myscan.nextDouble();
+                            b1.reqLoan(z);
+                        }
+                        case 4 -> {
+                            System.out.print("\n -> Enter the repay loan amount: ");
+                            double w = myscan.nextDouble();
+                            b1.repaySanctionedLoan(w);
+                        }
+                        case 5 -> {
+                            b1.inspection();
+                        }
+                        default -> {
+                            System.out.println("Please enter a correct choice.");
+                        }
                     }
 
+                    System.out.print("\n  > 1. Add money to your existing account.\n  > 2. Show details about your account.\n  > 3. Request for a loan.\n  > 4. Repay your loan.\n  > 5. Inspection.\n  > 6. Exit.");
+                    System.out.print("\n\n  > Enter your choice: ");
+                    y = myscan.nextInt();
+                }
+
+            }
+            else {
+                System.out.println("Please enter a correct choice.");
             }
 
-            System.out.print("\nPress 'Y' to create another account or 'N' to exit. ");
-            s = myscan.nextLine();
+            System.out.print("\n  > 1. Create an Account.\n  > 2. Exit");
+            System.out.print("\n\n  > Enter your choice: ");
+            x = myscan.nextInt();
 
         }
-
-
-
     }
-
-
 }
