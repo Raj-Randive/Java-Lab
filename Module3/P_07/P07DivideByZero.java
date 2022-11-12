3cb9c4d2e77d18373f4c499b87c060fdaefb6291
Raj Randive <randiveraj96@gmail.com>
2022-11-12 22:56:40 +0530

    initial commit

--- Module3/P_07/P07DivideByZero.java
+++ Module3/P_07/P07DivideByZero.java
@@ -1,10 +1,21 @@
 package Module3.P_07;
-import java.lang.Math;
+import java.util.Scanner;
 
 public class P07DivideByZero {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("\nEnter the first number (a): ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number (b): ");
+        int b = sc.nextInt();
 
+        try{
+            System.out.println("The division (a/b) is: " + (a/b));
+        }catch(ArithmeticException exp){
+//            System.out.println(exp.getMessage());
+            System.out.println("\n_____________________________\nDivide by Zero Exception.\n_____________________________");
+        }
 
     }
 }
