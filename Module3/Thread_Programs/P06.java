838bc92091b7b3c686907a99118f826b46c12848
Raj Randive <randiveraj96@gmail.com>
2022-12-04 23:37:27 +0530

    initial commit

---
+++ Module3/Thread_Programs/P06.java
@@ -0,0 +1,11 @@
+package Module3.Thread_Programs;
+
+public class P06 {
+    public static void main(String args[]){
+
+//        Thread obj = Thread.currentThread();
+        Thread obj = new Thread();
+        System.out.println("The current thread is : " + obj);
+
+    }
+}
